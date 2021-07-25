d914bbca79156c1a0f4775e656870a08dbe6411c
Subay <Subay421@gmail.com>
2021-07-25 09:31:44 +0530

    Initial Commit

---
+++ News Today/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News Today
+//
+//  Created by Subendran on 25/07/21.
+//  Copyright © 2021 Subendran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
